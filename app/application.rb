26e9f810346a3fe15d12cf608831217de605de49
Tyler Maxwell <tylermaxwell661@gmail.com>
2018-02-28 16:47:13 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,9 +1,20 @@
 class Application
-  
+
+  @@items = []
+
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
-    
-    
- end 
+
+    if req.path.match(/items/)
+
+      item_name = req.path.split("/items/").last
+      item = @@items.find{|s| s.name == item_name}
+      resp.write item.price
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+    resp.finish
+  end
 end
